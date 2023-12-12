f3d566620086b0ac7387f1b82780e49cbb4eca18
sachin-niyana <sachinjakhar100@gmail.com>
2023-12-12 11:24:45 +0530

    add gsap

--- assets/js/gsap3.js
+++ assets/js/gsap3.js
@@ -1,1 +1,14 @@
-jkhjkkkkk;
+gsap.from("#navlogo ul li a,#navlogo img", {
+  z: -80,
+  duration: 0.5,
+  opacity: 0,
+  delay: 0.5,
+  stagger: 0.3,
+});
+
+gsap.from("#rotate", {
+  rotate: 360,
+  duration: 5,
+  repeat: -1,
+  scrollTrigger: {},
+});
