673cbb46409ae7441b0c922f6a73630f22073f7b
sukhvinder-jaat <monudhaka829@gmail.com>
2023-12-13 10:37:17 +0530

    gsap added

--- assets/js/gsap2.js
+++ assets/js/gsap2.js
@@ -34,19 +34,19 @@ gsap.from("#burger .w_45px", {
     scrub: 1,
   },
 });
-gsap.to(".Img_center_md img", {
-  scale: 6,
-  opacity: 0,
-  duration: 0.5,
-  scrollTrigger: {
-    trigger: ".Img_center_md img",
-    scroller: "body",
-    // markers: true,
-    start: "90% 25%",
-    end: "230% 35%",
-    scrub: 1,
-  },
-});
+// gsap.to(".Img_center_md img", {
+//   scale: 6,
+//   opacity: 0,
+//   duration: 0.5,
+//   scrollTrigger: {
+//     trigger: ".Img_center_md img",
+//     scroller: "body",
+//     // markers: true,
+//     start: "90% 25%",
+//     end: "230% 35%",
+//     scrub: 1,
+//   },
+// });
 var tl3 = gsap.timeline({
   scrollTrigger: {
     trigger: "#section2 #para2,#section2 div img",
@@ -67,15 +67,54 @@ tl3.from("#section2 div img", {
   y: 100,
   x: 100,
 });
-// gsap.from("#imagesMan img", {
-//   x: "-100%",
+const tl4 = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#imagesMan",
+    start: "top center",
+    end: "bottom center",
+    scrub: 4,
+    // markers: true,
+  },
+});
+tl4.from("#imagesMan", {
+  x: "-100%",
+});
+tl4.to("#imagesMan img", {
+  x: 145,
+  stagger: 0.2,
+});
+// const timeline = gsap.timeline({
 //   scrollTrigger: {
-//     trigger: "#imageScroll img",
-//     scroller: "#imageScroll",
-//     markers: true,
-//     start: "60% 25%",
-//     end: "190% 50%",
-//     scrub: 1,
-//     pin: "#imagesMan",
+//     scrub: true,
+//     trigger: "#lineRed .timeline",
+//     start: "top top",
+//     end: "bottom bottom",
+//     pin: ".pinLine",
+//     onUpdate: (self) => {
+//       const newHeight = 100 + 710 * self.progress;
+//       document.querySelector(".timeline").style.height = `${newHeight}px`;
+//     },
 //   },
 // });
+gsap.to("#teamImages", {
+  scrollTrigger: {
+    trigger: "#teamSection",
+    start: "top center",
+    end: "bottom center",
+    scrub: true,
+  },
+  x: "-100%",
+  ease: "power1.out",
+});
+gsap.to("#timelineMain1 .timeline1", {
+  scrollTrigger: {
+    trigger: "#timelineMain1 .timeline1",
+    start: "top center",
+    end: "bottom 10%",
+    scrub: true,
+    markers: true,
+  },
+  duration: 5,
+  height: "100%",
+  ease: "power1.out",
+});
