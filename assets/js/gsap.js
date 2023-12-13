a5675b1e3108057776890e1c8b42e9c6ac6d3f8f
Dh-Pardeep <pardeepjangra514@gmail.com>
2023-12-13 10:46:52 +0530

    kkkkk

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -16,26 +16,24 @@ tl.from("#navlogo ul li, #navlogo div img", {
         stagger: 0.3,
         delay: 0.3
     })
-    .from("#logo div img", {
-        x: 80,
-        duration: 0.3,
+
+    .from("#popularised", {
+        x: -100,
+        y: -100,
         opacity: 0,
         stagger: 0.3,
-        delay: 0.3,
-        ease: "linear",
-        scrollTrigger:
-        {
-            trigger: "#logo",
+        scrollTrigger: {
+            trigger: "#popularised",
             scroller: "body",
-            start: "top center",
-            end: "bottom center",
+            start: "60% 85%",
+            end: "bottom 90%",
             markers: false,
             scrub: true
         }
     })
-    .from("#popularised", {
-        x: -50,
-        y: 100,
+    .from("#popularised2", {
+        x: 50,
+        y: -100,
         opacity: 0,
         stagger: 0.3,
         scrollTrigger: {
@@ -54,10 +52,10 @@ tl.from("#navlogo ul li, #navlogo div img", {
         ease: "linear"
     })
     .from("#Supplies", {
-        x: -80,
-        y: 100,
+        x: -200,
+        y: 200,
         opacity: 0,
-        stagger: 0.3,
+        stagger: 1.5,
         scrollTrigger: {
             trigger: "#Supplies",
             scroller: "body",
@@ -73,7 +71,7 @@ tl.from("#navlogo ul li, #navlogo div img", {
         opacity: 0,
         stagger: 0.3,
         scrollTrigger: {
-            trigger: "#Supplies",
+            trigger: "#roadmap",
             scroller: "body",
             start: "top center",
             end: "bottom center",
@@ -81,6 +79,19 @@ tl.from("#navlogo ul li, #navlogo div img", {
             scrub: true,
         }
     })
+gsap.to(".Img_center_md img", {
+    scale: 6,
+    opacity: 0,
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: ".Img_center_md img",
+        scroller: "body",
+        // markers: true,
+        start: "90% 25%",
+        end: "230% 35%",
+        scrub: 1,
+    },
+});
 
 // Define the animation
 tl.from(".grid", {
@@ -93,5 +104,22 @@ tl.from(".grid", {
         start: "top center",
         end: "bottom center",
         scrub: 1
+    }
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".slide");
+gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+        trigger: ".horizontal-sliders",
+        pin: ".main",
+        pinSpacing: true,
+        scrub: true,
+        end: "+=3000",
+
     }
 });
